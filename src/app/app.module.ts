830101fd545d8cbc9025bd681a75d835868513bc
Nakata <4ryo2-meron-math@ezweb.ne.jp>
2019-04-02 03:36:51 +0900

    add display selector

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TopComponent } from './home/top/top.component';
+import { ProblemComponent } from './home/problem/problem.component';
+import { SubmitComponent } from './home/submit/submit.component';
+import { RankComponent } from './home/rank/rank.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TopComponent,
+    ProblemComponent,
+    SubmitComponent,
+    RankComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+		AppRoutingModule,
+		FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
